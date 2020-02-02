4ae9ea15d6e39489b5d405f91c11a798d59ee249
Matthew Krueger <matthapkidokarate@gmail.com>
2020-02-02 17:39:50 -0500

    added internet test code
    and tweaked font

--- src/main/java/com/saunderstheaterproperties/utils/InternetTest.java
+++ src/main/java/com/saunderstheaterproperties/utils/InternetTest.java
@@ -4,7 +4,31 @@ import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
 
+import com.saunderstheaterproperties.utils.errors.GenericErrorDisplay;
+import com.saunderstheaterproperties.utils.errors.GenericErrorDisplay.GenericErrorSettings;
+
 public class InternetTest {
+	
+	
+	public static boolean performInternetTest() {
+		
+		boolean isConnected = isConnectedToInternet();
+		
+		if(!isConnected) {
+			try {
+				GenericErrorDisplay.getGenericErrorDisplay("No Internet Connection", "<html><body><h1>There is no internet connection available.</h1><br />"
+						+ "<p>While internet is required for this application, clicking ignore will attempt to continue running<br />"
+						+ "the program; however, the program will still not function correctly without internet.</p></body></html>", 
+						GenericErrorSettings.FATAL_RECOVER).LATCH.await();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			};
+		}
+		
+		return isConnected;
+		
+	}
 
 	
 	public static boolean isConnectedToInternet() {
